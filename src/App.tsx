8b084bf32fe3000a8647276a4bfb4556f4dd37dc
CalderonCodes <cgcalderondev@gmail.com>
2025-08-07 14:04:39 -0600

    :feat: add card component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.scss'
+import Card from './components/Card/Card'
 
 function App() {
 
   return (
     <div>
-      <h1></h1>
+      <h1>Desserts</h1>
+      <Card/>
     </div>
   )
 }
---
+++ src/components/Card/Card.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Card.scss";
+import add from "../../assets/icon-add-to-cart.svg";
+
+function Card() {
+  return (
+    <div className="card">
+      <img
+        className="card__image"
+        src="/src/assets/image-baklava-desktop.jpg"
+        alt=""
+      />
+      <button className="card__button">
+        <img src={add} alt="" />
+        <span>Add to cart</span>
+      </button>
+
+      <div className="card__body">
+        <p className="card__tag">Waffle</p>
+        <h1 className="card__name">Waffle with berries</h1>
+        <p className="card__price">$6.50</p>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
