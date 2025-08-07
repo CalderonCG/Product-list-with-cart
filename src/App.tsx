3fe61f6d4e6db0497fa8a59830f2f865be3a622e
CalderonCodes <cgcalderondev@gmail.com>
2025-08-07 14:21:59 -0600

    :feat: add card grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.scss'
-import Card from './components/Card/Card'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.scss";
+import Card from "./components/Card/Card";
 
 function App() {
-
   return (
-    <div>
-      <h1>Desserts</h1>
-      <Card/>
+    <div className="container">
+      <div className="container__dessert">
+        <h1>Desserts</h1>
+        <div className="container__list">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      </div>
+
+      <div className="container__cart">
+        <h1>Cart</h1>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
